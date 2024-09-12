9b1928485337c10667058675b3f8564b8cfb8971
Julio M Cruz <julio.cruz@eb-ms.net>
2024-09-12 01:20:27 -0400

    Calling Telegram App

--- DefiBotBackend/app/page.tsx
+++ DefiBotBackend/app/page.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import { createThirdwebClient } from "thirdweb";
+import { ConnectButton } from "thirdweb/react";
+
+
+export const client = createThirdwebClient({ clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "" });
+
+
 export default function Home() {
   // Check if window.TelegramGameProxy is available
   if (typeof window !== "undefined" && window.TelegramGameProxy) {
@@ -11,6 +18,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Welcome to Meta Pool Investment Bot</h1>
+      <ConnectButton client={client} />
     </div>
   );
 }
--- DefiBotBackend/next.config.mjs
+++ DefiBotBackend/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'encoding');
+    return config;
+  },
+  typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
         // your project has type errors.
